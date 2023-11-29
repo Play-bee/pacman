9e365127ded0204744dd69a0151d500cdab5a9e4
MaxiV95 <maximilianovanmegroot@gmail.com>
2023-11-29 12:43:45 -0300

    prueba evitar ERR_BLOCKED_BY_CLIENT

--- js/play-bee-functions.js
+++ js/play-bee-functions.js
@@ -16,7 +16,7 @@ function readUrlParams() {
 	return false;
 }
 
-async function sendResult(score, level, timePlayed) {
+function sendResult(score, level, timePlayed) {
 	const currentDate = new Date();
 	const gameStats = {
 		playerId: PLAYER_ID,
@@ -26,15 +26,12 @@ async function sendResult(score, level, timePlayed) {
 		date: currentDate.toISOString(),
 	};
 	console.log("Juego finalizado", gameStats);
-	const response = await fetch("https://localhost:8000/tournaments/game-over", {
-    method: "POST",
-		mode: "cors",
-		credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(gameStats),
-  });
-  const data = await response.json();
-  console.log("Response:", data);
+	fetch("http://34.28.220.88:8000/tournaments/game-over", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(gameStats),
+	})
+		.then((response) => response.json())
+		.then((data) => console.log({data}))
+		.catch((error) => console.error({error}));
 }
